b777566140248d91bb5fef35837913549468bb20
Alex Rajvandary <alex.rajvandary@mostgames.org>
2023-03-05 01:40:26 +0545

    LevenshteinDistance algorithm added

--- VacanciesAnalyzerHH/MainViewModel.cs
+++ VacanciesAnalyzerHH/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -118,10 +119,16 @@ namespace VacanciesAnalyzerHH
             TotalNumberOfVacancies = deserializedData.found.Value;
             TotalNumberOfPages = deserializedData.pages.Value;
 
+            GetSkills();
+        }
+
+        public void GetSkills()
+        {
             if (Vacancies.Count > 0)
             {
                 var skills = Vacancies.Select(v => v.snippet.requirement);
                 var t = new List<string>();
+                var dict = new Dictionary<string, List<string>>();
                 foreach (var skill in skills)
                 {
                     if (skill == null)
@@ -129,12 +136,61 @@ namespace VacanciesAnalyzerHH
                         continue;
                     }
 
-                    var s = skill.Split(new string[] { ";", "Знание", "," }, options: System.StringSplitOptions.RemoveEmptyEntries);
+                    var s = skill.ToLower().Split(new string[] { ";", "уверенное", "знание", "опыт", "уметь", "знать", ",", ". " }, options: System.StringSplitOptions.TrimEntries);
                     t.AddRange(s);
+                }
+
+                t.RemoveAll(w => string.IsNullOrWhiteSpace(w));
+                var num = t.Count;
+                for (int i = 0; i < t.Count; i++)
+                {
+                    if (dict.TryAdd(t[i], new List<string> { t[i] }))
+                    {
+                        for (int j = i + 1; j < t.Count; j++)
+                        {
+                            if (LevenshteinDistance(t[i], t[j]) < 5 || (t[i].Length >= t[j].Length ? t[i].Contains(t[j]) : t[j].Contains(t[i])))
+                            {
+                                if (t[i].Length <= 5 && !(t[i].Length >= t[j].Length ? t[i].Contains(t[j]) : t[j].Contains(t[i])))
+                                {
+                                    continue;
+                                }
+
+                                dict[t[i]].Add(t[j]);
+                                t.RemoveAt(j);
+                                j--;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
                 }
 
                 Skills = new ObservableCollection<string>(t);
             }
+        }
+
+        public int LevenshteinDistance(string string1, string string2)
+        {
+            if (string1 == null) throw new ArgumentNullException("string1");
+            if (string2 == null) throw new ArgumentNullException("string2");
+            int diff;
+            int[,] m = new int[string1.Length + 1, string2.Length + 1];
+
+            for (int i = 0; i <= string1.Length; i++) { m[i, 0] = i; }
+            for (int j = 0; j <= string2.Length; j++) { m[0, j] = j; }
+
+            for (int i = 1; i <= string1.Length; i++)
+            {
+                for (int j = 1; j <= string2.Length; j++)
+                {
+                    diff = (string1[i - 1] == string2[j - 1]) ? 0 : 1;
+
+                    m[i, j] = Math.Min(Math.Min(m[i - 1, j] + 1, m[i, j - 1] + 1), m[i - 1, j - 1] + diff);
+                }
+            }
+            return m[string1.Length, string2.Length];
         }
 
         private void OnPropertyChanged([CallerMemberName] string paramName = null)
