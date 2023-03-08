05b4dc4a11c803989a7904136463065e09d016ec
Alex Rajvandary <alex.rajvandary@mostgames.org>
2023-03-08 21:08:56 +0545

    Created view model for data filtering.

---
+++ VacanciesAnalyzerHH/DataFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacanciesAnalyzerHH
+{
+    public class DataFilter
+    {
+        private string adress;
+        private string companyName;
+        private string contacts;
+        private string name;
+        private string published;
+        private double? salaryFrom;
+        private double? salaryTo;
+        private string schedule;
+    }
+}
